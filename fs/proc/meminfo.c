677e8748b658de0b40f8f3a8735e5700e737de36
Tong <123toorc@gmail.com>
2020-04-22 14:02:41 -0400

    Updated
    unsupport CONFIG_TRANSPARENT_HUGEPAGE memory track

--- fs/proc/meminfo.c
+++ fs/proc/meminfo.c
@@ -71,7 +71,9 @@ static int meminfo_proc_show(struct seq_file *m, void *v)
 		pages[lru] = global_node_page_state(NR_LRU_BASE + lru);
 
 	available = si_mem_available();
+
 #ifndef CONFIG_POPCORN_REMOTE_INFO
+
 	show_val_kb(m, "MemTotal:       ", i.totalram);
 	show_val_kb(m, "MemFree:        ", i.freeram);
 	show_val_kb(m, "MemAvailable:   ", available);
@@ -161,126 +163,96 @@ static int meminfo_proc_show(struct seq_file *m, void *v)
 		    global_zone_page_state(NR_FREE_CMA_PAGES));
 #endif
 
-#else // CONFIG_POPCORN_REMOTE_INFO
-	remote_proc_mem_info(&rem_mem);
+#else //CONFIG_POPCORN_REMOTE_INFO
 
-	seq_printf(m,
-		"MemTotal:       %8lu kB\n"
-		"MemFree:        %8lu kB\n"
-		"MemAvailable:   %8lu kB\n"
-		"Buffers:        %8lu kB\n"
-		"Cached:         %8lu kB\n"
-		"SwapCached:     %8lu kB\n"
-		"Active:         %8lu kB\n"
-		"Inactive:       %8lu kB\n"
-		"Active(anon):   %8lu kB\n"
-		"Inactive(anon): %8lu kB\n"
-		"Active(file):   %8lu kB\n"
-		"Inactive(file): %8lu kB\n"
-		"Unevictable:    %8lu kB\n"
-		"Mlocked:        %8lu kB\n"
+	show_val_kb(m, "MemTotal:       ", i.totalram + rem_mem.MemTotal);
+	show_val_kb(m, "MemFree:        ", i.freeram + rem_mem.MemFree);
+	show_val_kb(m, "MemAvailable:   ", available + rem_mem.MemAvailable);
+	show_val_kb(m, "Buffers:        ", i.bufferram + rem_mem.Buffers);
+	show_val_kb(m, "Cached:         ", cached + rem_mem.Cached);
+	show_val_kb(m, "SwapCached:     ", total_swapcache_pages() + rem_mem.SwapCached);
+	show_val_kb(m, "Active:         ", pages[LRU_ACTIVE_ANON] +
+					   pages[LRU_ACTIVE_FILE] + rem_mem.Active);
+	show_val_kb(m, "Inactive:       ", pages[LRU_INACTIVE_ANON] +
+					   pages[LRU_INACTIVE_FILE] + rem_mem.Inactive);
+	show_val_kb(m, "Active(anon):   ", pages[LRU_ACTIVE_ANON] + rem_mem.Active_anon);
+	show_val_kb(m, "Inactive(anon): ", pages[LRU_INACTIVE_ANON] + rem_mem.Inactive.anon);
+	show_val_kb(m, "Active(file):   ", pages[LRU_ACTIVE_FILE] + rem_mem.Active_file);
+	show_val_kb(m, "Inactive(file): ", pages[LRU_INACTIVE_FILE] + rem_mem.Inactive_file);
+	show_val_kb(m, "Unevictable:    ", pages[LRU_UNEVICTABLE] + rem_mem.Unevictable);
+	show_val_kb(m, "Mlocked:        ", global_zone_page_state(NR_MLOCK) + rem_mem.Mlocked);
+
 #ifdef CONFIG_HIGHMEM
-		"HighTotal:      %8lu kB\n"
-		"HighFree:       %8lu kB\n"
-		"LowTotal:       %8lu kB\n"
-		"LowFree:        %8lu kB\n"
+	show_val_kb(m, "HighTotal:      ", i.totalhigh + rem_mem.HighTotal);
+	show_val_kb(m, "HighFree:       ", i.freehigh + rem_mem.HighFree);
+	show_val_kb(m, "LowTotal:       ", i.totalram - i.totalhigh + rem_mem.LowTotal);
+	show_val_kb(m, "LowFree:        ", i.freeram - i.freehigh + rem_mem.LowFree);
 #endif
+
 #ifndef CONFIG_MMU
-		"MmapCopy:       %8lu kB\n"
+	show_val_kb(m, "MmapCopy:       ",
+		    (unsigned long)atomic_long_read(&mmap_pages_allocated) + rem_mem.MmapCopy);
 #endif
-		"SwapTotal:      %8lu kB\n"
-		"SwapFree:       %8lu kB\n"
-		"Dirty:          %8lu kB\n"
-		"Writeback:      %8lu kB\n"
-		"AnonPages:      %8lu kB\n"
-		"Mapped:         %8lu kB\n"
-		"Shmem:          %8lu kB\n"
-		"Slab:           %8lu kB\n"
-		"SReclaimable:   %8lu kB\n"
-		"SUnreclaim:     %8lu kB\n"
-		"KernelStack:    %8lu kB\n"
-		"PageTables:     %8lu kB\n"
+
+	show_val_kb(m, "SwapTotal:      ", i.totalswap + rem_mem.SwapTotal);
+	show_val_kb(m, "SwapFree:       ", i.freeswap + rem_mem.SwapFree);
+	show_val_kb(m, "Dirty:          ",
+		    global_node_page_state(NR_FILE_DIRTY) + rem_mem.Dirty);
+	show_val_kb(m, "Writeback:      ",
+		    global_node_page_state(NR_WRITEBACK) + rem_mem.Writeback);
+	show_val_kb(m, "AnonPages:      ",
+		    global_node_page_state(NR_ANON_MAPPED) + rem_mem.AnonPages);
+	show_val_kb(m, "Mapped:         ",
+		    global_node_page_state(NR_FILE_MAPPED) + rem_mem.Mapped);
+	show_val_kb(m, "Shmem:          ", i.sharedram + rem_mem.Shmem);
+	show_val_kb(m, "Slab:           ",
+		    global_node_page_state(NR_SLAB_RECLAIMABLE) +
+		    global_node_page_state(NR_SLAB_UNRECLAIMABLE) + rem_mem.Slab);
+
+	show_val_kb(m, "SReclaimable:   ",
+		    global_node_page_state(NR_SLAB_RECLAIMABLE) + rem_mem.SReclaimable);
+	show_val_kb(m, "SUnreclaim:     ",
+		    global_node_page_state(NR_SLAB_UNRECLAIMABLE) + rem_mem.SUnreclaim);
+	seq_printf(m, "KernelStack:    %8lu kB\n",
+		   global_zone_page_state(NR_KERNEL_STACK_KB) + rem_mem.KernelStack);
+	show_val_kb(m, "PageTables:     ",
+		    global_zone_page_state(NR_PAGETABLE) + rem_mem.PageTables);
 #ifdef CONFIG_QUICKLIST
-		"Quicklists:     %8lu kB\n"
+	show_val_kb(m, "Quicklists:     ", quicklist_total_size() + rem_mem.Quicklists);
 #endif
-		"NFS_Unstable:   %8lu kB\n"
-		"Bounce:         %8lu kB\n"
-		"WritebackTmp:   %8lu kB\n"
-		"CommitLimit:    %8lu kB\n"
-		"Committed_AS:   %8lu kB\n"
-		"VmallocTotal:   %8lu kB\n"
-		"VmallocUsed:    %8lu kB\n"
-		"VmallocChunk:   %8lu kB\n"
+
+	show_val_kb(m, "NFS_Unstable:   ",
+		    global_node_page_state(NR_UNSTABLE_NFS) + rem_mem.NFS_Unstable);
+	show_val_kb(m, "Bounce:         ",
+		    global_zone_page_state(NR_BOUNCE) + rem_mem.Bounce);
+	show_val_kb(m, "WritebackTmp:   ",
+		    global_node_page_state(NR_WRITEBACK_TEMP) + rem_mem.WritebackTmp);
+	show_val_kb(m, "CommitLimit:    ", vm_commit_limit() + rem_mem.CommitLimit);
+	show_val_kb(m, "Committed_AS:   ", committed + rem_mem.Committed_AS);
+	seq_printf(m, "VmallocTotal:   %8lu kB\n",
+		   ((unsigned long)VMALLOC_TOTAL >> 10) + rem_mem.VmallocTotal);
+	show_val_kb(m, "VmallocUsed:    ", 0ul);
+	show_val_kb(m, "VmallocChunk:   ", 0ul);
+
 #ifdef CONFIG_MEMORY_FAILURE
-		"HardwareCorrupted: %5lu kB\n"
+	seq_printf(m, "HardwareCorrupted: %5lu kB\n",
+		   (atomic_long_read(&num_poisoned_pages) << (PAGE_SHIFT - 10)) 
+		   + rem_mem.HardwareCorrupted);
 #endif
+
 #ifdef CONFIG_TRANSPARENT_HUGEPAGE
-		"AnonHugePages:  %8lu kB\n"
+	show_val_kb(m, "AnonHugePages:  ",
+		    global_node_page_state(NR_ANON_THPS) * HPAGE_PMD_NR);
+	show_val_kb(m, "ShmemHugePages: ",
+		    global_node_page_state(NR_SHMEM_THPS) * HPAGE_PMD_NR);
+	show_val_kb(m, "ShmemPmdMapped: ",
+		    global_node_page_state(NR_SHMEM_PMDMAPPED) * HPAGE_PMD_NR);
 #endif
+
 #ifdef CONFIG_CMA
-		"CmaTotal:       %8lu kB\n"
-		"CmaFree:        %8lu kB\n"
-#endif
-		,
-		K(i.totalram) + rem_mem.MemTotal,
-		K(i.freeram) + rem_mem.MemFree,
-		K(available) + rem_mem.MemAvailable,
-		K(i.bufferram) + rem_mem.Buffers,
-		K(cached) + rem_mem.Cached,
-		K(total_swapcache_pages()) + rem_mem.SwapCached,
-		K(pages[LRU_ACTIVE_ANON]   + pages[LRU_ACTIVE_FILE]) + rem_mem.Active,
-		K(pages[LRU_INACTIVE_ANON] + pages[LRU_INACTIVE_FILE]) + rem_mem.Inactive,
-		K(pages[LRU_ACTIVE_ANON]) + rem_mem.Active_anon,
-		K(pages[LRU_INACTIVE_ANON]) + rem_mem.Inactive_anon,
-		K(pages[LRU_ACTIVE_FILE]) + rem_mem.Active_file,
-		K(pages[LRU_INACTIVE_FILE]) + rem_mem.Inactive_file,
-		K(pages[LRU_UNEVICTABLE]) + rem_mem.Unevictable,
-		K(global_page_state(NR_MLOCK)) + rem_mem.Mlocked,
-#ifdef CONFIG_HIGHMEM
-		K(i.totalhigh) + rem_mem.HighTotal,
-		K(i.freehigh) + rem_mem.HighFree,
-		K(i.totalram-i.totalhigh) + rem_mem.LowTotal,
-		K(i.freeram-i.freehigh) + rem_mem.LowFree,
-#endif
-#ifndef CONFIG_MMU
-		K((unsigned long) atomic_long_read(&mmap_pages_allocated)) + rem_mem.MmapCopy,
-#endif
-		K(i.totalswap) + rem_mem.SwapTotal,
-		K(i.freeswap) + rem_mem.SwapFree,
-		K(global_page_state(NR_FILE_DIRTY)) + rem_mem.Dirty,
-		K(global_page_state(NR_WRITEBACK)) + rem_mem.Writeback,
-		K(global_page_state(NR_ANON_PAGES)) + rem_mem.AnonPages,
-		K(global_page_state(NR_FILE_MAPPED)) + rem_mem.Mapped,
-		K(i.sharedram) + rem_mem.Shmem,
-		K(global_page_state(NR_SLAB_RECLAIMABLE) +
-				global_page_state(NR_SLAB_UNRECLAIMABLE)) + rem_mem.Slab,
-		K(global_page_state(NR_SLAB_RECLAIMABLE)) + rem_mem.SReclaimable,
-		K(global_page_state(NR_SLAB_UNRECLAIMABLE)) + rem_mem.SUnreclaim,
-		global_page_state(NR_KERNEL_STACK) * THREAD_SIZE / 1024 + rem_mem.KernelStack,
-		K(global_page_state(NR_PAGETABLE)) + rem_mem.PageTables,
-#ifdef CONFIG_QUICKLIST
-		K(quicklist_total_size()) + rem_mem.Quicklists,
-#endif
-		K(global_page_state(NR_UNSTABLE_NFS)) + rem_mem.NFS_Unstable,
-		K(global_page_state(NR_BOUNCE)) + rem_mem.Bounce,
-		K(global_page_state(NR_WRITEBACK_TEMP)) + rem_mem.WritebackTmp,
-		K(vm_commit_limit()) + rem_mem.CommitLimit,
-		K(committed) + rem_mem.Committed_AS,
-		((unsigned long)VMALLOC_TOTAL >> 10) + rem_mem.VmallocTotal,
-		0ul, // used to be vmalloc 'used'
-		0ul  // used to be vmalloc 'largest_chunk'
-#ifdef CONFIG_MEMORY_FAILURE
-		, (atomic_long_read(&num_poisoned_pages) << (PAGE_SHIFT - 10)) + rem_mem.HardwareCorrupted
-#endif
-#ifdef CONFIG_TRANSPARENT_HUGEPAGE
-		, K(global_page_state(NR_ANON_TRANSPARENT_HUGEPAGES) *
-		   HPAGE_PMD_NR) + rem_mem.AnonHugePages
-#endif
-#ifdef CONFIG_CMA
-		, K(totalcma_pages) + rem_mem.CmaTotal
-		, K(global_page_state(NR_FREE_CMA_PAGES)) + rem_mem.CmaFree
-#endif
-		);
+	show_val_kb(m, "CmaTotal:       ", totalcma_pages + rem_mem.CmaTotal);
+	show_val_kb(m, "CmaFree:        ",
+		    global_zone_page_state(NR_FREE_CMA_PAGES) + rem_mem.CmaFree);
 #endif
 
 	hugetlb_report_meminfo(m);
